bde6c16dc81f15ef1afd45b5aa5c3bc36e3c22dc
cHr1s0Gil <qudcksrlf1@gmail.com>
2026-05-15 14:10:20 +0900

    initialize project

---
+++ src/main/java/com/example/eai_shipment_api/EaiShipmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.eai_shipment_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaiShipmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaiShipmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eaishipmentapi/EaiShipmentApiApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.eaishipmentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaiShipmentApiApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
